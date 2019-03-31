b399b017fa3a59fc78169a46d3c0534aeabb4022
leejongcheal <aksndk123@naver.com>
2019-04-01 03:00:09 +0900

    commit1 알림창 생성

--- bull_eye2/ViewController.swift
+++ bull_eye2/ViewController.swift
@@ -14,7 +14,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showAlert(){
+        // 버튼 눌렀을때 나오는 화면
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: "This is my first app!",
+                                      preferredStyle: .alert)
+        //밑에 버튼 부분
+        let action = UIAlertAction(title: "Awesome", style: .default,
+                                   handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
 
 }
 
