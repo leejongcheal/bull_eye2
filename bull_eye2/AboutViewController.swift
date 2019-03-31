e82c689aa73aa22e7d73e8299b483b3bb19c2740
leejongcheal <aksndk123@naver.com>
2019-04-01 04:46:12 +0900

    commit6 새로운 뷰 화면을 위해서 파일을 추가한뒤에 view controller	을 추가해줌 그리고 close에 대해서 액션 정의 해주기

---
+++ bull_eye2/AboutViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AboutViewController.swift
+//  bull_eye2
+//
+//  Created by 캡디 on 01/04/2019.
+//  Copyright © 2019 lee. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated:true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
